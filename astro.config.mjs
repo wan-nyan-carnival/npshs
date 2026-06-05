3607173a9820adada67c314f33ed27a3b4d7610f
kakeru <flyingsheep085@gmail.com>
2026-06-05 18:01:20 +0900

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  // ↓ GitHub Pages用の設定を追加
-  site: 'https://wan-nyan-carnival.github.io', // 必ずあなたのユーザー名に置き換えてください
-  base: '/npshs', // 必ずあなたのリポジトリ名に置き換えてください
-  // ↑ ここまで追加
+  site: 'https://wan-nyan-carnival.github.io',
+  base: '/npshs/',
 });

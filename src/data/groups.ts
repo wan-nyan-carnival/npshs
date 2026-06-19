4a7b81ac7d79b8960402c1169f4c3572c33b8e98
kakeru <flyingsheep085@gmail.com>
2026-06-19 18:50:53 +0900

    フッターの追加など

--- src/data/groups.ts
+++ src/data/groups.ts
@@ -1,12 +1,17 @@
 export const pokemonGroups = [
   { id: "park", label: "公園" },
-  { id: "camp", label: "ベースキャンプ" },
   { id: "jungle", label: "ジャングル" },
-  { id: "forest", label: "樹海" },
+  { id: "sands", label: "砂漠" },
   { id: "beach", label: "ビーチ" },
   { id: "reef", label: "海上" },
   { id: "undersea", label: "海中" },
-  { id: "sands", label: "砂漠" },
+  { id: "camp", label: "ベースキャンプ" },
   { id: "volcano", label: "火山" },
+  { id: "forest", label: "樹海" },
   { id: "snowfields", label: "雪原" },
+/*  { id: "cave", label: "洞窟" },
+  { id: "ruins", label: "遺跡" },
+  { id: "sidepath", label: "抜け道" },
+  { id: "river", label: "リバー" },
+  { id: "badlands", label: "荒野" },*/
 ];
